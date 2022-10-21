738f343622ecaf6a16f10aa9491a353904698633
FLY123 <akfakf0509@naver.com>
2022-10-21 11:15:30 +0900

    work card(WIP)

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup>
+const workList = ref(["asdf"]);
+</script>
+
 <template>
+    <ul>
+        <WorkCard title="test" />
+        <WorkCard title="test" />
+        <WorkCard title="test" />
+        <WorkCard title="test" />
+    </ul>
     <div>Hello World</div>
+    {{ workList }}
 </template>
---
+++ components/WorkCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    title: {
+        default: "",
+        required: false,
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <li class="work-card">
+        <p class="work-card__title">{{ title }}</p>
+        이것은 내용이여
+    </li>
+</template>
+
+<style lang="scss" scoped>
+.work-card {
+    &__title {
+        color: red;
+    }
+}
+</style>
