405748387c52bf1bead18726ab830e42200bca7b
FLY123 <akfakf0509@naver.com>
2022-11-07 02:12:02 +0900

    feat: 스타일 및 데이터 동기화

--- app.vue
+++ app.vue
@@ -4,11 +4,36 @@ import { Props as WorkCardProps } from "~/components/WorkCard.vue";
 
 const workList: Ref<WorkCardProps[]> = ref([]);
 
-workList.value.push({ isDone: true, title: "1" });
-workList.value.push({ isDone: false, title: "2" });
-workList.value.push({ isDone: false, title: "3" });
-workList.value.push({ isDone: true, title: "4" });
-workList.value.push({ isDone: false, title: "5" });
+workList.value.push({
+    content: "This is content",
+    id: 1,
+    isDone: true,
+    title: "1",
+});
+workList.value.push({
+    content: "This is content",
+    id: 2,
+    isDone: false,
+    title: "2",
+});
+workList.value.push({
+    content: "This is content",
+    id: 3,
+    isDone: false,
+    title: "3",
+});
+workList.value.push({
+    content: "This is content",
+    id: 4,
+    isDone: true,
+    title: "4",
+});
+workList.value.push({
+    content: "This is content",
+    id: 5,
+    isDone: false,
+    title: "5",
+});
 
 function handleChange(i: number, isDone: boolean) {
     workList.value.splice(i, 1, { ...workList.value[i], isDone });
@@ -16,7 +41,7 @@ function handleChange(i: number, isDone: boolean) {
 </script>
 
 <template>
-    <ul>
+    <ul class="list">
         <WorkCard
             v-for="(work, i) in workList"
             v-bind="work"
@@ -24,3 +49,11 @@ function handleChange(i: number, isDone: boolean) {
         />
     </ul>
 </template>
+
+<style lang="scss" scoped>
+.list {
+    display: grid;
+    gap: 8px;
+    padding: 8px;
+}
+</style>
