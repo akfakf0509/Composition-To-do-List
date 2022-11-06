b24a8ebef6a1b3aaf0c3917c87862c21e3903043
FLY123 <akfakf0509@naver.com>
2022-11-07 00:16:34 +0900

    feat: Props 및 emit 정리

--- app.vue
+++ app.vue
@@ -1,14 +1,27 @@
-<script setup>
-const workList = ref(["asdf"]);
+<script setup lang="ts">
+import { Ref } from "vue";
+import { Props as WorkCardProps } from "~/components/WorkCard.vue";
+
+const workList: Ref<WorkCardProps[]> = ref([]);
+
+workList.value.push({ id: 1, isDone: true, title: "1" });
+workList.value.push({ id: 2, isDone: false, title: "2" });
+workList.value.push({ id: 3, isDone: false, title: "3" });
+workList.value.push({ id: 4, isDone: true, title: "4" });
+workList.value.push({ id: 5, isDone: false, title: "5" });
+
+function handleChange(id, isDone) {
+    console.log(id, isDone);
+}
 </script>
 
 <template>
     <ul>
-        <WorkCard title="test" />
-        <WorkCard title="test" />
-        <WorkCard title="test" />
-        <WorkCard title="test" />
+        <WorkCard
+            v-for="work in workList"
+            v-bind="work"
+            @change="handleChange"
+        />
     </ul>
-    <div>Hello World</div>
     {{ workList }}
 </template>
--- components/WorkCard.vue
+++ components/WorkCard.vue
@@ -1,15 +1,25 @@
-<script setup>
-const props = defineProps({
-    title: {
-        default: "",
-        required: false,
-        type: String,
-    },
-});
+<script setup lang="ts">
+export interface Props {
+    id: number;
+    title: string;
+    isDone: boolean;
+}
+export interface Emits {
+    (e: "change", id: number, isDone: boolean): void;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits<Emits>();
+const { id, isDone, title } = toRefs(props);
+
+function emitChange() {
+    emit("change", id.value, !isDone.value);
+}
 </script>
 
 <template>
     <li class="work-card">
+        <input type="checkbox" :checked="isDone" @change="emitChange" />
         <p class="work-card__title">{{ title }}</p>
         이것은 내용이여
     </li>
