9c01089bac5917c7609c5cd57e4e8fed43431d4a
FLY123 <akfakf0509@naver.com>
2022-11-07 13:16:37 +0900

    feat: work composable -> types

---
+++ stores/work.ts
@@ -0,0 +1,28 @@
+import { defineStore } from "pinia";
+import Work from "~~/types/Work";
+
+export const useWorkStore = defineStore("work", () => {
+    const idIndex = ref(0);
+    const workList = ref<Work[]>([]);
+
+    function updateIsDone(index: number, isDone: boolean) {
+        workList.value.splice(index, 1, { ...workList.value[index], isDone });
+    }
+    function pushWork(
+        content: Work["content"] = "",
+        isDone: Work["isDone"] = false,
+        title: Work["title"] = ""
+    ) {
+        workList.value.push({
+            content,
+            id: idIndex.value++,
+            isDone,
+            title,
+        });
+    }
+    function removeWork(index: number) {
+        workList.value.splice(index, 1);
+    }
+
+    return { idIndex, workList, updateIsDone, pushWork, removeWork };
+});
