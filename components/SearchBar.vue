f4130d54788eaf7fae117a779d3fdd9b5817587c
FLY123 <akfakf0509@naver.com>
2022-11-07 14:37:25 +0900

    feat: SearchBar 추가

---
+++ components/SearchBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+export interface Emits {
+    (e: "search", value: string): void;
+}
+
+const emit = defineEmits<Emits>();
+const searchText = ref("");
+</script>
+
+<template>
+    <form class="search-bar" @submit.prevent="$emit('search', searchText)">
+        <TextInput v-model="searchText" class="search-bar__input" />
+        <button class="search-bar__button">검색</button>
+    </form>
+</template>
