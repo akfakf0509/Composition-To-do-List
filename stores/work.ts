418beb963c21aff3705f8b221c96874838d1175c
FLY123 <akfakf0509@naver.com>
2022-11-13 21:20:50 +0900

    feat: sortedWorkList, custom sort 방식 변경

--- stores/work.ts
+++ stores/work.ts
@@ -9,11 +9,12 @@ export const useWorkStore = defineStore("work", () => {
     const customIndexMap = ref<Record<number, number>>({});
 
     const sortedWorkList = computed<Work[]>(() => {
+        const copyWorkList = Array.from(workList.value);
         if (sort.value === "custom")
-            return workList.value.sort((firstWork, lastWork) =>
+            return copyWorkList.sort((firstWork, lastWork) =>
                 sortByCustom(firstWork, lastWork, customIndexMap.value)
             );
-        return workList.value.sort(sortMap[sort.value]);
+        return copyWorkList.sort(sortMap[sort.value]);
     });
 
     function updateIsDone(index: number, state: State) {
@@ -25,23 +26,44 @@ export const useWorkStore = defineStore("work", () => {
         });
     }
     function updateSort(newSort: Sort) {
+        if (newSort === "custom") resetCustomIndexMap();
         sort.value = newSort;
+    }
+    function updateCustomIndexMap(id: number, targetId: number) {
+        const targetIndex = customIndexMap.value[targetId];
+        customIndexMap.value[targetId] = customIndexMap.value[id];
+        customIndexMap.value[id] = targetIndex;
     }
     function pushWork(
         content: Work["content"] = "",
         state: Work["state"] = "todo",
         title: Work["title"] = ""
     ) {
-        workList.value.push({
+        const newWork: Work = {
             id: idIndex.value++,
             title,
             content,
             createdDate: new Date(),
             state,
-        });
+        };
+        workList.value.push(newWork);
+        if (sort.value === "custom") pushCustomIndexMap(newWork);
     }
     function removeWork(index: number) {
-        workList.value.splice(index, 1);
+        const removedWork = workList.value.splice(index, 1);
+        if (sort.value === "custom") removeCustomIndexMap(removedWork[0]);
+    }
+
+    function pushCustomIndexMap(work: Work) {
+        customIndexMap.value[work.id] = workList.value.length - 1;
+    }
+    function removeCustomIndexMap(work: Work) {
+        delete customIndexMap.value[work.id];
+    }
+    function resetCustomIndexMap() {
+        workList.value.forEach(
+            (work, i) => (customIndexMap.value[work.id] = i)
+        );
     }
 
     return {
@@ -52,6 +74,7 @@ export const useWorkStore = defineStore("work", () => {
         sortedWorkList,
         updateIsDone,
         updateSort,
+        updateCustomIndexMap,
         pushWork,
         removeWork,
     };
